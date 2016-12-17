7555997b29afdf1d5f1f05101f3b1e3bd5c40bb9
Jeff Wolfram <jeffwolfram@gmail.com>
2016-12-16 16:32:56 -0800

    I think I did it all

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,18 @@
-var app = angular.module("sampleApp", ["firebase"]);
+(function() {
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA43vCSBA4_79187AbPH9NnxHmH9O8Ir0Y",
+    authDomain: "bloc-chat-51e80.firebaseapp.com",
+    databaseURL: "https://bloc-chat-51e80.firebaseio.com",
+    storageBucket: "bloc-chat-51e80.appspot.com",
+    messagingSenderId: "1018101314147"
+  };
+  firebase.initializeApp(config);
+
+})();
+
+
+
+
+
+var app = angular.module("bloc-chat", ["firebase"], ['ui.router']);
